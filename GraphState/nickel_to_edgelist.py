869157c07de7190439cbce193c9cfa4e1dc2462a
Viktor Papara <papara@mpp.mpg.de>
2015-05-15 15:06:59 +0200

    Went back to usage of "nickel_to_edgelist.py" with arguments. Now the input is just a string given to the script as an argument: ./nickel_to_edgelist.py "e111|ee|:0_1_1_3|11|"

--- GraphState/nickel_to_edgelist.py
+++ GraphState/nickel_to_edgelist.py
@@ -7,18 +7,19 @@
 #   (((0,), ), ((0, 1), ), ((0, 1), ), ((1,), ))
 
 import sys
-import fileinput
 import graph_state
 
 
-for name_raw in fileinput.input():
-    name = name_raw.strip()
-    edge_property_key = graph_state.PropertyKey(name="color", is_edge_property=True, is_directed=False, externalizer=graph_state.PropertyExternalizer())
-    colored_edges_config = graph_state.PropertiesConfig.create(edge_property_key)
-    gs = colored_edges_config.graph_state_from_str(name)
-    nickel_ = str(gs)
+name = sys.argv[1]
 
-    if name <> nickel_:
-        raise ValueError, "Non mininal nickel index %s, minimal = %s" % (name, nickel_)
+edge_property_key = graph_state.PropertyKey(name="color", is_edge_property=True, is_directed=False, externalizer=graph_state.PropertyExternalizer())
+
+colored_edges_config = graph_state.PropertiesConfig.create(edge_property_key)
+
+gs = colored_edges_config.graph_state_from_str(name)
+nickel_ = str(gs)
+
+if name <> nickel_:
+    raise ValueError, "Non mininal nickel index %s, minimal = %s" % (name, nickel_)
     
-    print gs.edges
+print gs.edges
